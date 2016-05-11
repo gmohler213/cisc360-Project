98bbf3257e0f24a78db08859db455ea52161b26c
Greg <gmohler@udel.edu>
2016-05-11 17:18:15 +0000

    tried to create atax cpp

---
+++ polybench-c-4.1/linear-algebra/kernels/atax/atax.preproc.c
@@ -0,0 +1,24 @@
+/**
+ * This version is stamped on Apr. 14, 2015
+ *
+ * Contact:
+ *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
+ *   Tomofumi Yuki <tomofumi.yuki.fr>
+ *
+ * Web address: http://polybench.sourceforge.net
+ */
+/* atax.c: this file is part of PolyBench/C */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+/* Include polybench common header. */
+#include<polybench.h>
+# 1 "atax.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "atax.c"
